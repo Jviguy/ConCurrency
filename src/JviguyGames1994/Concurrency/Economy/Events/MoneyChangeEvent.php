b7ccfa9b6577a08bcd22d04d8b7146063978628e
mmm545 <allhammhmd83@gmail.com>
2020-08-18 00:40:37 +0300

    Added setter for uuid and PHPDocs

--- src/JviguyGames1994/Concurrency/Economy/Events/MoneyChangeEvent.php
+++ src/JviguyGames1994/Concurrency/Economy/Events/MoneyChangeEvent.php
@@ -31,7 +31,20 @@ class MoneyChangeEvent extends Event implements Cancellable
 	public function getChange(): float{
 		return $this->change;
 	}
-	public function setChange(float $change){
-		$this->change = $change;
-	}
+
+    /**
+     * @param float $change
+     * @return void sets the change in money
+     */
+    public function setChange(float $change): void{
+        $this->change = $change;
+    }
+
+    /**
+     * @param string $uuid
+     * @return void sets the players uuid that changed amount
+     */
+    public function setUUID(string $uuid): void{
+        $this->uuid = $uuid;
+    }
 }
