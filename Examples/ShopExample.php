e02df4ddbb14e0bd7fb1889d2497f7698c629ffb
jviguy <jviguytwo2@gmail.com>
2020-08-17 16:29:54 -0400

    Added on to the ShopExample with event listening

--- Examples/ShopExample.php
+++ Examples/ShopExample.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 use JviguyGames1994\Concurrency\Concurrency;
 use JviguyGames1994\Concurrency\Economy\EconomyHandlers;
+use JviguyGames1994\Concurrency\Economy\Events\AddMoneyEvent;
+use JviguyGames1994\Concurrency\Economy\Events\MoneyChangeEvent;
 
 class ShopExample
 {
@@ -14,7 +16,21 @@ class ShopExample
 			//error thrown because that economy doesnt exist
 		}
 		$player = "player"; // player class
-		$emmaskills = $Kills->get($player->getUUID());
+		try {
+			$emmaskills = $Kills->get($player->getUUID());
+		} catch (InvalidArgumentException $exception){
+			//error thrown because emma isnt in the economy data base
+		}
 		$Kills->sum($player-getUUID(), -1000);// subtracts 1000 from emmas kills because the sum function follows interger rules
+	}
+	public function onAdd(MoneyChangeEvent $event){
+		if ($event instanceof AddMoneyEvent){
+			//called when a player is added money
+		}else if ($event instanceof \JviguyGames1994\Concurrency\Economy\Events\SubtractMoneyEvent){
+			$changeinmoney = $event->getChange();
+			if ($changeinmoney < -1000){
+				$event->setCancelled();//stops the money from changing
+			}
+		}
 	}
 }
