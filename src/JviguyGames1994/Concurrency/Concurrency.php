56ce9baa05d85ab5ba4f5906f7f4edcf7e02b712
jviguy <jviguytwo2@gmail.com>
2020-08-17 17:22:08 -0400

    removed testing stuff

--- src/JviguyGames1994/Concurrency/Concurrency.php
+++ src/JviguyGames1994/Concurrency/Concurrency.php
@@ -14,13 +14,6 @@ class Concurrency extends PluginBase{
 	public function onEnable()
 	{
 		self::$instance = new EconomyHandlers($this);
-		$starting = 0; $max = PHP_INT_MAX;
-		self::$instance->registerNewEconomy(new RoundedEconomy($starting,$max), "Money");
-		try {
-			$economygroup = self::$instance->getEconomy("Money");
-			} catch (InvalidArgumentException $exception){
-				$this->getLogger()->info($exception->getMessage());
-		}
 	}
 	public static function getInstance(){
 		return self::$instance;
