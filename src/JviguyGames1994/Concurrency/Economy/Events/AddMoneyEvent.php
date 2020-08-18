4b12812dba62ae68b102fe42d4c3ab440e2f017e
Carson <51865762+BoomYourBang@users.noreply.github.com>
2020-08-18 00:25:44 -0400

    Update AddMoneyEvent.php

--- src/JviguyGames1994/Concurrency/Economy/Events/AddMoneyEvent.php
+++ src/JviguyGames1994/Concurrency/Economy/Events/AddMoneyEvent.php
@@ -9,5 +9,8 @@ class AddMoneyEvent extends MoneyChangeEvent
 	public function __construct(string $uuid, int $change)
 	{
 		parent::__construct($uuid, $change);
+		if($change < 0){
+			$this->setCancelled();
+		}
 	}
 }
